be7323543f2399732f916e91c4ffbaa9e7e2c070
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-03-14 07:56:00 +0545

    made server offline

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../styles/Home.css'
+import '../styles/home.css'
 import { Container, Row, Col } from 'reactstrap'
 import heroImg from '../assets/images/hero-img01.jpg'
 import heroImg02 from '../assets/images/hero-img02.jpg'
