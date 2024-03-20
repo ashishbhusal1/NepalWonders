3b3c6b78bd40d0957bd9c17aec7d7b7be52b12a6
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-03-20 18:51:14 +0545

    edited user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,10 +4,12 @@ import {
   getAllUser,
   getSingleUser,
   updateUser,
+  createUser
 } from "../controllers/userController.js";
 import { verifyUser ,verifyAdmin} from "../utils/verifyToken.js";
 
 const router = express.Router();
+router.post("/", verifyAdmin, createUser);
 router.put("/:id", verifyUser,updateUser);
 router.delete("/:id", verifyUser,deleteUser);
 router.get("/:id", verifyUser, getSingleUser);
