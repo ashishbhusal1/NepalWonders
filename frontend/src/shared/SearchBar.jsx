046f0483f954078056be66a230817b5ae82aede9
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-04-05 21:33:12 +0545

    Added linear search with accumulation algorithm

--- frontend/src/shared/SearchBar.jsx
+++ frontend/src/shared/SearchBar.jsx
@@ -11,7 +11,7 @@ const SearchBar = () => {
    const navigate = useNavigate()
 
    const searchHandler = async() => {
-      const location = locationRef.current.value
+      const location = locationRef.current.value.toLowerCase() // Convert location to lowercase
       const distance = distanceRef.current.value
       const maxGroupSize = maxGroupSizeRef.current.value
 
@@ -21,32 +21,34 @@ const SearchBar = () => {
 
       const res = await fetch(`${BASE_URL}/tours/search/getTourBySearch?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`)
       
-      if(!res.ok) alert('Something went wrong')
+      if(!res.ok) {
+         return alert('Something went wrong')
+      }
 
       const result = await res.json()
 
-      navigate(`/tours/search?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`, {state: result.data})
+      navigate(`/tours/search?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`, { state: result.data })
    }
 
    return <Col lg="12">
       <div className="search__bar">
          <Form className='d-flex align-items-center gap-4'>
             <FormGroup className='d-flex gap-3 form__group form__group-fast'>
-               <span><i class='ri-map-pin-line'></i></span>
+               <span><i className='ri-map-pin-line'></i></span>
                <div>
                   <h6>Location</h6>
                   <input type="text" placeholder='Where are you going?' ref={locationRef} />
                </div>
             </FormGroup>
             <FormGroup className='d-flex gap-3 form__group form__group-fast'>
-               <span><i class='ri-map-pin-time-line'></i></span>
+               <span><i className='ri-map-pin-time-line'></i></span>
                <div>
                   <h6>Distance</h6>
                   <input type="number" placeholder='Distance k/m' ref={distanceRef} />
                </div>
             </FormGroup>
             <FormGroup className='d-flex gap-3 form__group form__group-last'>
-               <span><i class='ri-group-line'></i></span>
+               <span><i className='ri-group-line'></i></span>
                <div>
                   <h6>Max People</h6>
                   <input type="number" placeholder='0' ref={maxGroupSizeRef} />
@@ -54,7 +56,7 @@ const SearchBar = () => {
             </FormGroup>
 
             <span className='search__icon' type='submit' onClick={searchHandler}>
-               <i class='ri-search-line'></i>
+               <i className='ri-search-line'></i>
             </span>
          </Form>
       </div>
