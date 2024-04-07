f63733e3ee71f948bdef460f67d345c40342e9da
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-04-08 00:24:56 +0545

    Added css to gallery

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 import galleryImages from '../components/Image-gallery/galleryImage'
+import '../styles/gallery.css'
 
 const Gallery = () => {
    return (
+      <div className="wrapper">
       <ResponsiveMasonry columnsCountBreakPoints={{350:1, 768:1, 992:4}}>
-         <Masonry gutter='1rem'>
+         <Masonry gutter='1rem' >
             {
                galleryImages.map((item, index) => (
                   <img className='masonry__img' src={item} key={index} alt="" 
@@ -14,6 +16,7 @@ const Gallery = () => {
             }
          </Masonry>
       </ResponsiveMasonry>
+      </div>
    )
 }
 
