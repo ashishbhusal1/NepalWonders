4e18c8d45a147dbfe8925883a286ec13b5f5cd69
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-04-08 23:07:07 +0545

    added links to footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -47,22 +47,22 @@ const Footer = () => {
                      <p>"Start your adventure. Get travel tips and deals when you sign up. Your journey begins with us."</p>
                      <div className="social__link d-flex align-items-center gap-4">
                         <span>
-                           <Link to='#'>
+                           <Link to='https://www.youtube.com/channel/UCjcvv5aO7O7wlZMQ4sWqcdw'>
                               <i class='ri-youtube-line'></i>
                            </Link>
                         </span>
                         <span>
-                           <Link to='#'>
+                           <Link to='https://github.com/ashishbhusal1/NepalWonders'>
                               <i class='ri-github-fill'></i>
                            </Link>
                         </span>
                         <span>
-                           <Link to='#'>
+                           <Link to='https://www.facebook.com/'>
                               <i class='ri-facebook-circle-line'></i>
                            </Link>
                         </span>
                         <span>
-                           <Link to='#'>
+                           <Link to='https://www.instagram.com/'>
                               <i class='ri-instagram-line'></i>
                            </Link>
                         </span>
@@ -105,7 +105,7 @@ const Footer = () => {
                            <span><i class='ri-map-pin-line'></i></span>
                            Address:
                         </h6>
-                        <p className='mb-0'>Bharatpur-11,Chitwan</p>
+                        <p className='mb-0'>Bharatpur, Chitwan</p>
                      </ListGroupItem>
 
                      <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-3'>
@@ -114,7 +114,7 @@ const Footer = () => {
                            Email:
                         </h6>
 
-                        <p className='mb-0'>NepalWonders@gmail.com</p>
+                        <p className='mb-0'>nepalwonders@gmail.com</p>
                      </ListGroupItem>
 
                      <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-3'>
@@ -123,7 +123,7 @@ const Footer = () => {
                            Phone:
                         </h6>
 
-                        <p className='mb-0'>+9779812345679</p>
+                        <p className='mb-0'>+977-9811103266</p>
                      </ListGroupItem>
                   </ListGroup>
                </Col>
