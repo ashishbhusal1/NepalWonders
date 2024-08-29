c01d2d61bb4efd4e966fb24d3c3febc24aadd1bf
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-08-29 19:13:59 +0545

    few changes

---
+++ backend/api/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import tourRoute from "./routes/tours.js";
+import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
+import reviewRoute from "./routes/reviews.js";
+import bookingRoute from "./routes/bookings.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
+mongoose.set("strictQuery", false);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log("MongoDB connection failed:", error);
+  }
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/tours", tourRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/review", reviewRoute);
+app.use("/api/v1/booking", bookingRoute);
+
+// Connect to MongoDB
+connect();
+
+// Instead of app.listen, export the app for Vercel
+export default app;
--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,31 +1,37 @@
-import express from 'express'
-import { createTour, deleteTour, getAllTour, getFeaturedTour, getSingleTour, getTourBySearch, getTourCount, updateTour } from '../Controllers/tourControllers.js'
+import express from "express";
+import {
+  createTour,
+  deleteTour,
+  getAllTour,
+  getFeaturedTour,
+  getSingleTour,
+  getTourBySearch,
+  getTourCount,
+  updateTour,
+} from "../Controllers/tourControllers.js";
 
-import { verifyAdmin } from '../utils/verifyToken.js'
+import { verifyAdmin } from "../utils/verifyToken.js";
 
-const router = express.Router()
+const router = express.Router();
 
-//Create new tour 
-router.post('/', verifyAdmin, createTour)
+//Create new tour
+router.post("/", verifyAdmin, createTour);
 
-//Update tour 
-router.put('/:id', verifyAdmin, updateTour)
+//Update tour
+router.put("/:id", verifyAdmin, updateTour);
 
-//Delete tour 
-router.delete('/:id', verifyAdmin, deleteTour)
+//Delete tour
+router.delete("/:id", verifyAdmin, deleteTour);
 
-//Get single tour 
-router.get('/:id', getSingleTour)
+//Get single tour
+router.get("/:id", getSingleTour);
 
-//Get all tour 
-router.get('/', getAllTour)
+//Get all tour
+router.get("/", getAllTour);
 
 //Get tour by search
-router.get("/search/getTourBySearch", getTourBySearch)
-router.get("/search/getFeaturedTour", getFeaturedTour)
-router.get("/search/getTourCount", getTourCount)
+router.get("/search/getTourBySearch", getTourBySearch);
+router.get("/search/getFeaturedTour", getFeaturedTour);
+router.get("/search/getTourCount", getTourCount);
 
-
-
-
-export default router
+export default router;
