956239350da2cfc3b58e60d6528b36186f41d5a0
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-08-29 19:29:19 +0545

    small fix

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,8 @@ import { login, register, checkUsernameAvailability } from '../Controllers/authC
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/check-username', checkUsernameAvailability); // New route for checking username availability
+router.post('/api/v1/auth/register', register);
+router.post('/api/v1/auth/login', login);
+router.post('/api/v1/auth/check-username', checkUsernameAvailability); // New route for checking username availability
 
 export default router;
